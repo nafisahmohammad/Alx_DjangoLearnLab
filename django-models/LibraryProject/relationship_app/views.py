dde028e92ae6304cb9ca866a6193d0a550183c8a
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-18 23:31:11 +0100

    Fix views template paths and structure for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,11 @@ from .models import Book, Library
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 # Class-based view: library detail
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
