875fd87362f079255a56e3ce29a038e08fb246a6
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-18 23:43:45 +0100

    Fix urls import to satisfy checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from relationship_app.views import list_books
-from relationship_app.views import LibraryDetailView
+from .views import list_books
+from .views import LibraryDetailView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
+
 
 
