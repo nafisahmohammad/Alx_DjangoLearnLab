89da0217d16c7dbfd58effb64a6c1e13d66850dc
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-19 22:40:10 +0100

    Add book_list view and permissions README

---
+++ advanced_features_and_security/LibraryProject/README.md
@@ -0,0 +1,13 @@
+# Managing Permissions and Groups in Django
+
+This project demonstrates how to manage permissions and groups in Django.
+
+## Features
+- Custom model permissions (can_view, can_create, can_edit, can_delete)
+- Permission enforcement using the permission_required decorator
+- Group-based access control via Django admin
+
+## Usage
+Assign users to groups (Admins, Editors, Viewers) in the Django admin
+and grant appropriate permissions to control access to views.
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,24 +1,23 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
 
-# Permissions enforcement for Book actions
+# Permission-based book views
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_book(request):
-    return HttpResponse("You can view books")
+def book_list(request):
+    return HttpResponse("List of books")
 
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    return HttpResponse("You can create books")
+    return HttpResponse("Create book")
 
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request):
-    return HttpResponse("You can edit books")
+    return HttpResponse("Edit book")
 
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request):
-    return HttpResponse("You can delete books")
-
+    return HttpResponse("Delete book")
